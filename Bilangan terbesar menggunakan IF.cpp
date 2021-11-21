328f63812ee7b7a57e5cb855aeea0285301d52ec
Hey_rezza <49313466+Rezza3004@users.noreply.github.com>
2021-11-21 22:33:03 +0700

    Add files via upload

---
+++ Bilangan terbesar menggunakan IF.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B,C,D;
+    cout<<"\t\t Mencari Bilangan Terbesar terdiri dari 4 Angka"<<endl<<endl;
+    cout<<"Bilangan 1: ";
+    cin>>A;
+    cout<<"Bilangan 2: ";
+    cin>>B;
+    cout<<"Bilangan 3: ";
+    cin>>C;
+    cout<<"Bilangan 4: ";
+    cin>>D;
+               
+        if (A>B)
+            if (A>C)
+                if (A>D)
+                cout<<"Bilangan Terbesar adalah "<<A;
+                else
+                cout<<"Bilangan Terbesar adalah "<<D;
+                else
+                if (C>D)
+                cout<<"Bilangan Terbesar adalah "<<C;
+                else
+                cout<<"Bilangan Terbesar adalah "<<D;               
+        else
+                if(B>C)
+                   if(B>D)
+                	cout<<"Bilangan Terbesar adalah "<<B;
+                    else
+                    cout<<"Bilangan Terbesar adalah "<<D;
+                    else
+                    if(C>D)
+                    cout<<"Bilangan Terbesar adalah "<<C;
+                    else
+                    cout<<"Bilangan Terbesar adalah "<<D;
+}
